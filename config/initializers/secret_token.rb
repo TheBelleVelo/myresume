dc42b0e1673a89382c43fe83d30447fea27f9816
Simon Masters <simonmasters@echelontek.com>
2013-07-26 13:34:38 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myresume::Application.config.secret_token = 'b2131a1cd1257e2b3d4296f1e491bb60812b8502f1eb89d3558468d76f2f1cede4b0c590dfbb7c362317c285dcfd3c554ab30acc3e838bdaf15fc5784b7590bf'
